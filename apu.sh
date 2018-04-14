332022be03acbd2e8a6a1b979b11b64cb86dc3f6
controllinghand <s@controllinghand.com>
2018-04-13 20:51:03 -0700

    added Path
    Added Path

--- apu.sh
+++ apu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # apu.sh
 SHELL=/bin/bash
-PATH=$PATH:/usr/sbin
+PATH=$PATH:/usr/sbin:/usr/local/sbin:/sbin
 # This script updates OS patches
 exec > >(tee -i /root/apu/apuoutput.log)
 apt-get update
